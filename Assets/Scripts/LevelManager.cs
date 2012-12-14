46457f5c424f9a68b7e704d30e17ddafaadccbd8
Maxime Houlier <m.houlier@gmail.com>
2012-12-14 20:47:39 +0100

    Added base script for level manager.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	private bool m_LoadingLevel = true;
+	
+	// Use this for initialization
+	void Start()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	void OnLevelWasLoaded(int _level)
+	{
+		m_LoadingLevel = false;
+	}
+	
+	void LoadLevel(int _level)
+	{
+		Application.LoadLevel(_level);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+	
+	void OnGUI()
+	{
+		//Loading screen
+		if (m_LoadingLevel)
+		{
+			
+		}
+	}
+}
