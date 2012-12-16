5c4363621f91c3c4e75b9ef87eac2c6fb0b99d91
Engleek <engleek@gmail.com>
2012-12-16 23:47:00 +0100

    Added "esc" pause menu,

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -2,8 +2,40 @@ using UnityEngine;
 using System.Collections;
 
 public class GameMenu : MonoBehaviour {
-
-	void OnGUI () {
-		GUI.Box(new Rect(Screen.width / 2 - 80, Screen.height / 2 - 20, 160, 40), "Paused");
+	
+	private bool m_GamePaused = false;
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown("escape"))
+		{
+			if (m_GamePaused)
+			{
+				m_GamePaused = false;
+				//AudioListener.volume = 1;
+				Time.timeScale = 1;
+				//Screen.showCursor = false;
+			}
+			else
+			{
+				m_GamePaused = true;
+				//AudioListener.volume = 0;
+				Time.timeScale = 0;
+				//Screen.showCursor = true;				
+			}
+		}
+	}
+	
+	void OnGUI ()
+	{
+		if (m_GamePaused)
+		{
+			GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 20, 200, 120), "Paused");
+	
+			if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2 + 30, 180, 50), "Quit Game"))
+			{
+				Application.Quit();
+			}
+		}
 	}
 }
