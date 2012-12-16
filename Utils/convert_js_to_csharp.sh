97f4be95fcbee6e8221e9997a825a618d843da5d
Maxime Houlier <m.houlier@gmail.com>
2012-12-16 19:09:14 +0100

    Improved *.js to *.cs file converter so that it can run all steps at once if no step index is provided on the command line.

--- Utils/convert_js_to_csharp.sh
+++ Utils/convert_js_to_csharp.sh
@@ -2,47 +2,57 @@
 
 InputFile="$1"
 OutputFile="$2"
-TmpFile="$2.tmp"
+TmpFileBase="$2"
 echo "converting $InputFile to $OutputFile"
 
 #sed s/eth0/eth1/g file1 >  file2
 
-if [ $3 -eq 1 ]; then
-    sed 's/var \(.*\) : \(.*\) = \(.*\);/\2 \1 = \3;/g' $InputFile > $TmpFile
-else
-if [ $3 -eq 2 ]; then
-    sed 's/var \(.*\) : \(.*\);/\2 \1;/g' $InputFile > $TmpFile
-else
-if [ $3 -eq 3 ]; then
-    sed 's/public static function \(.*\)(\(.*\)) : \(.*\) \(.*\)/public static \3 \1(\2) \4/g' $InputFile > $TmpFile
-else
-if [ $3 -eq 4 ]; then
-    sed 's/function \(.*\)(\(.*\)) : \(.*\) \(.*\)/public \3 \1(\2) \4/g' $InputFile > $TmpFile
-else
-if [ $3 -eq 5 ]; then
-    sed 's/\([a-zA-Z0-9_\-]*\) : \([a-zA-Z0-9_\-]*\)\([,)]\)/\2 \1\3/g' $InputFile > $TmpFile
-else
-if [ $3 -eq 6 ]; then
-    sed 's/function \(.*\)/public void \1/g' $InputFile > $TmpFile
-else
-if [ $3 -eq 7 ]; then
-    sed 's/boolean/bool/g' $InputFile > $TmpFile
-else
-if [ $3 -eq 8 ]; then
-    echo "using UnityEngine;" > $TmpFile
-    echo "using System.Collections;" >> $TmpFile
-    echo "" >> $TmpFile
-    echo "public class PlayerMovementMotor_new : MonoBehaviour {" >> $TmpFile
-    sed 's/\(.*\)/    \1/g' $InputFile | sed 's/#pragma strict//g' >> $TmpFile
-    echo "}" >> $TmpFile
-fi  #8
-fi  #7
-fi  #6
-fi  #5
-fi  #4
-fi  #3
-fi  #2
-fi  #1
+ TmpInputFile="$InputFile"
 
-mv $TmpFile $OutputFile
+if [ $# -eq 2 ] || [ $3 -eq 1 ]; then
+    TmpOutputFile="${TmpFileBase}_1"
+    sed 's/var \(.*\) : \(.*\) = \(.*\);/\2 \1 = \3;/g' $TmpInputFile > $TmpOutputFile
+    TmpInputFile="$TmpOutputFile"
+fi
+if [ $# -eq 2 ] || [ $3 -eq 2 ]; then
+    TmpOutputFile="${TmpFileBase}_2"
+    sed 's/var \(.*\) : \(.*\);/\2 \1;/g' $TmpInputFile > $TmpOutputFile
+    TmpInputFile="$TmpOutputFile"
+fi
+if [ $# -eq 2 ] || [ $3 -eq 3 ]; then
+    TmpOutputFile="${TmpFileBase}_3"
+    sed 's/public static function \(.*\)(\(.*\)) : \(.*\) \(.*\)/public static \3 \1(\2) \4/g' $TmpInputFile > $TmpOutputFile
+    TmpInputFile="$TmpOutputFile"
+fi
+if [ $# -eq 2 ] || [ $3 -eq 4 ]; then
+    TmpOutputFile="${TmpFileBase}_4"
+    sed 's/function \(.*\)(\(.*\)) : \(.*\) \(.*\)/public \3 \1(\2) \4/g' $TmpInputFile > $TmpOutputFile
+    TmpInputFile="$TmpOutputFile"
+fi
+if [ $# -eq 2 ] || [ $3 -eq 5 ]; then
+    TmpOutputFile="${TmpFileBase}_5"
+    sed 's/\([a-zA-Z0-9_\-]*\) : \([a-zA-Z0-9_\-]*\)\([,)]\)/\2 \1\3/g' $TmpInputFile > $TmpOutputFile
+    TmpInputFile="$TmpOutputFile"
+fi
+if [ $# -eq 2 ] || [ $3 -eq 6 ]; then
+    TmpOutputFile="${TmpFileBase}_6"
+    sed 's/function \(.*\)/public void \1/g' $TmpInputFile > $TmpOutputFile
+    TmpInputFile="$TmpOutputFile"
+fi
+if [ $# -eq 2 ] || [ $3 -eq 7 ]; then
+    TmpOutputFile="${TmpFileBase}_7"
+    sed 's/boolean/bool/g' $TmpInputFile > $TmpOutputFile
+    TmpInputFile="$TmpOutputFile"
+fi
+if [ $# -eq 2 ] || [ $3 -eq 8 ]; then
+    TmpOutputFile="${TmpFileBase}_8"
+    echo "using UnityEngine;" > $TmpOutputFile
+    echo "using System.Collections;" >> $TmpOutputFile
+    echo "" >> $TmpOutputFile
+    echo "public class PlayerMovementMotor_new : MonoBehaviour {" >> $TmpOutputFile
+    sed 's/\(.*\)/    \1/g' $TmpInputFile | sed 's/#pragma strict//g' >> $TmpOutputFile
+    echo "}" >> $TmpOutputFile
+fi
+
+mv $TmpOutputFile $OutputFile
 
