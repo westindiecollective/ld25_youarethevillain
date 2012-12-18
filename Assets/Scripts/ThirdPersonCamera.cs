57a1cfcc568e0810e980196b5bb127f347b6fa67
Maxime Houlier <m.houlier@gmail.com>
2012-12-18 19:33:18 +0100

    Reworked ThirdPersonCamera so that a specific look direction can be specified instead of looking directly at the target. Using 'Use Look Dir As Follow Position Mask', camera position can now be only affected along the look direction. All this is used to provide a camera which translate along one dir following the player. This is probably not working for side scrolling/ travelling style camera moves => will look into it at a later time.

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -3,28 +3,44 @@ using System.Collections;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-	public float distanceAway;			// distance from the back of the craft
-	public float distanceUp;			// distance above the craft
-	public float smooth;				// how smooth the camera movement is
+	public float m_DistanceAway;		// distance from the back of the craft
+	public float m_DistanceUp;			// distance above the craft
+	public float m_SmoothFactor;		// how smooth the camera movement is
 	
-	private GameObject hovercraft;		// to store the hovercraft
-	private Vector3 targetPosition;		// the position the camera is trying to be in
+	public bool m_UseLookDir = false;
+	public bool m_UseLookDirAsFollowPositionMask = false;
+	public Vector3 m_LookDir = Vector3.forward;
+
+	private Vector3 m_TargetPosition;	// the position the camera is trying to be in
 	
-	Transform follow;
+	Transform m_FollowTransform;
 	
-	void Start(){
-		follow = GameObject.FindWithTag ("Player").transform;	
+	void Start()
+	{
+		m_FollowTransform = GameObject.FindWithTag ("Player").transform;
 	}
 	
-	void LateUpdate ()
+	void LateUpdate()
 	{
-		// setting the target position to be the correct offset from the hovercraft
-		targetPosition = follow.position + Vector3.up * distanceUp + follow.forward * distanceAway;
-		
-		// making a smooth transition between it's current position and the position it wants to be in
-		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+		float deltaTime = Time.deltaTime;
 		
 		// make sure the camera is looking the right way!
-		transform.LookAt(follow);
+		if (m_UseLookDir)
+		{
+			transform.rotation = Quaternion.LookRotation(m_LookDir);
+		}
+		else
+		{
+			transform.LookAt(m_FollowTransform);
+		}
+
+		Vector3 lookDir = (m_UseLookDir)? m_LookDir : m_FollowTransform.forward;
+		Vector3 followPos = (m_UseLookDirAsFollowPositionMask)? Vector3.Dot(m_FollowTransform.position, m_LookDir) * m_LookDir: m_FollowTransform.position;
+
+		// setting the target position to be the correct offset from the hovercraft
+		m_TargetPosition = followPos + m_DistanceUp * Vector3.up  - m_DistanceAway * lookDir;
+
+		// making a smooth transition between it's current position and the position it wants to be in
+		transform.position = Vector3.Lerp(transform.position, m_TargetPosition, deltaTime * m_SmoothFactor);
 	}
 }
