f6d1aa955d70e0311d46b4ce1d647f3a1d11273b
Maxime Houlier <m.houlier@gmail.com>
2013-03-11 17:39:54 +0100

    First pass on main menu rework. Menu will start only valid scenes.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,37 +3,109 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+	public GameObject m_GameManagerPrefab = null;
+	private LevelManager m_LevelManager = null;
+	
+	private bool m_IsActiveSequence = false;
+	
+	private int m_NextLevelIndex = -1;
+	
 	public int m_CreditSceneIndex = 0;
 	public int m_FirstLevelSceneIndex = 0;
+	
+	void Start()
+	{
+		StartSequence();
+	}
+	
+	void OnLevelWasLoaded(int _level)
+	{
+		//InitGameSequence();
+	}
+	
+	void StartSequence()
+	{
+		LevelManager levelManager = (LevelManager)FindObjectOfType( typeof(LevelManager) );
+		if (levelManager == null && m_GameManagerPrefab != null)
+		{
+			GameObject gameManager = Instantiate(m_GameManagerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+			levelManager = gameManager.GetComponent<LevelManager>();
+			
+			Debug.Log("Instantiating gameManager!");
+		}
+		
+		m_LevelManager = levelManager;
+		
+		m_IsActiveSequence = (m_LevelManager != null);
+		
+		InitCamera();
+	}
+	
+	void EndSequence()
+	{
+		m_IsActiveSequence = false;
+			
+		m_LevelManager.LoadLevel(m_NextLevelIndex);
+	}
+	
+	void InitCamera()
+	{
+		//GameObject mainCameraObject = GameObject.FindWithTag("MainCamera");
+		Camera mainCamera = Camera.main;
+		mainCamera.orthographic = true;
+	}
+	
+	void QuitGame()
+	{
+		m_LevelManager.QuitGame();
+	}
+	
+	void TransitionToLevel(int _LevelIndex)
+	{
+		m_NextLevelIndex = _LevelIndex;
+	}
+	
+	void UpdateMenuTransition(float _DeltaTime)
+	{
+		if ( m_LevelManager.IsValidLevelIndex(m_NextLevelIndex) )
+		{
+			EndSequence();
+		}
+	}
+	
+	void Update()
+	{
+		float deltaTime = Time.deltaTime;
+		
+		if (m_IsActiveSequence)
+		{
+			UpdateMenuTransition(deltaTime);
+		}
+	}
 
 	void OnGUI ()
 	{
-		GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 200, 200, 400), "\n<b>You are the villain</b>\n\nExcellent, you've kidnapped\nthe princess!\n\nDon't let the idiot hero\n get her back though...");
-		
-		if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height / 2, 160, 40), "Play"))
+		if (m_IsActiveSequence)
 		{
-			// Play Game
-			LevelManager instance = (LevelManager)FindObjectOfType(typeof(LevelManager));
-			if (instance)
+			GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 200, 200, 400), "\n<b>You are the villain</b>\n\nExcellent, you've kidnapped\nthe princess!\n\nDon't let the idiot hero\n get her back though...");
+			
+			if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height / 2, 160, 40), "Play"))
 			{
-				instance.LoadLevel(m_FirstLevelSceneIndex);
+				// Play Game
+				TransitionToLevel(m_FirstLevelSceneIndex);
 			}
-		}
-
-		if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height / 2 + 60, 160, 40), "About"))
-		{
-			// About Game
-			LevelManager instance = (LevelManager)FindObjectOfType(typeof(LevelManager));
-			if (instance)
+	
+			if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height / 2 + 60, 160, 40), "About"))
 			{
-				instance.LoadLevel(m_CreditSceneIndex);
+				// About Game
+				TransitionToLevel(m_CreditSceneIndex);
 			}
-		}
-
-		if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height / 2 + 120, 160, 40), "Exit"))
-		{
-			// Exit Game
-			Application.Quit();
+	
+			if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height / 2 + 120, 160, 40), "Exit"))
+			{
+				// Exit Game
+				QuitGame();
+			}
 		}
 	}
 }
